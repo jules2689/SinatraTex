1b6fcb70930b0716fb0bd5f20d4d91ac3e091ee6
Julian Nadeau <julian@jnadeau.ca>
2017-02-28 09:35:14 -0500

    Fix URL

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.7.2'
 
 set :application, 'sinatratex'
-set :repo_url, 'git@example.com:jules2689/sinatratex.git'
+set :repo_url, 'git@github.com:jules2689/sinatratex.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
